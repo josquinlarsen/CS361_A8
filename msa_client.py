40061aec51979e832be57c72beece5c4f9fbb736
Josquin Larsen <jsqnlrsn@gmail.com>
2024-11-08 21:05:40 -0500

    first commit

---
+++ msa_client.py
@@ -0,0 +1,49 @@
+import zmq
+import sys
+
+# ZMQ layout based on Luis Flores' Introduction to ZeroMQ (CS361 OSU)
+
+def main():
+    """
+    Runs the client logic for the ZeroMQ
+    Client socket connection
+    """
+
+    context = zmq.Context()
+    print("Connecting to server...")
+    # request socket
+    socket = context.socket(zmq.REQ)
+
+    #  '*' may need to be updated to localhost or 127.0.0.1
+    #  Port number may be altered to avoid collisions
+    socket.connect("tcp://127.0.0.1:5555")
+    print('Sending request...')
+
+    
+    while True:
+        artist_name = ""
+        if len(artist_name) < 1:
+            artist_name = input("Please enter an artist's name to search. Enter 'QUIT' to end: ")
+
+        # might need this to receive data from Main Program?
+        # will need to remove while loop if so.
+        # if len(sys.argv < 2:
+        #   print("Please provide an artist's name")
+        #   sys.exit(1)
+        #  artist_name = " ".join(sys.argv[1:])
+
+        socket.send_string(artist_name)
+
+        message = socket.recv()
+
+        if message.decode() == 'Goodbye!':
+            print('Goodbye!')
+            sys.exit()
+            break
+
+        print(f"Here's what I've found: {message.decode()}")
+
+
+if __name__ == "__main__":
+    main()
+
---
+++ msa_server.py
@@ -0,0 +1,56 @@
+import zmq
+import wikipediaapi
+
+# WikipediaAPI calls taken from https://pypi.org/project/Wikipedia-API/
+
+
+wiki_wiki = wikipediaapi.Wikipedia('Microservice A', 'en')
+
+
+def get_wikipedia_page(artist_name):
+    """
+    Searches Wikipedia for artist's name
+    If found, returns page,
+    otherwise returns error message
+    """
+    
+    page_search = wiki_wiki.page(artist_name)
+    if page_search.exists():
+        return page_search.fullurl
+    
+    else:
+        return f"I couldn't find a page for: {artist_name}. Check your spelling and punctation and try again."
+    
+def main():
+    """
+    Server side ZMQ 
+    """
+
+    context = zmq.Context()
+    # Response socket
+    socket = context.socket(zmq.REP)
+    #  May need to change * to local host or 127.0.0.1
+    socket.bind("tcp://127.0.0.1:5555")
+
+    print("Server is running...")
+
+    while True:
+        message = socket.recv()
+        artist_name = message.decode()
+        
+        if len(artist_name) > 0:
+            if artist_name == "QUIT":
+                print('Goodbye!')
+                socket.send_string('Goodbye!')
+                break
+
+            response_message = get_wikipedia_page(artist_name)
+            socket.send_string(response_message)
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+    
